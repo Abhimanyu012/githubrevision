4e36e85c1949b01532448fe871e1485e6bc3ee5e
Abhimanyu <abhimanyucode2021@gmail.com>
2024-09-29 15:48:50 +0530

    array end

---
+++ array2.js
@@ -0,0 +1,38 @@
+const marvel_heros = ["spiderman", "ironman" , "thor" ]
+
+const dc_heros = ["superman", "batman" , "flash"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros)
+// console.log(marvel_heros[3][1])
+
+//  const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros)
+
+// const allHeros =[...marvel_heros, ...dc_heros]
+// console.log(allHeros)
+// const anotherArray  = [1,2,3,[4,5,6],7,[8,9,8,[4,5]]]
+
+// const real_another_Array = anotherArray.flat(Infinity)
+
+// console.log(real_another_Array)
+
+   //  create array of anytype
+// console.log(Array.from("Abhimanyu"))
+// output
+// [
+//     'A', 'b', 'h',
+//     'i', 'm', 'a',
+//     'n', 'y', 'u'
+//   ]
+
+// convert into array
+
+// let score1 = 500
+// let score2 = 500
+// let score3= 500
+// let score4 = 500
+// console.log(Array.of(score1,score2,score3,score4))
+// // output
+// // [ 500, 500, 500, 500 ]
