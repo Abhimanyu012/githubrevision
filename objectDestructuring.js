c1009c658e2001354aba10d2d35a2d8d601c2d82
Abhimanyu <abhimanyucode2021@gmail.com>
2024-09-29 19:44:16 +0530

    object json

---
+++ objectDestructuring.js
@@ -0,0 +1,21 @@
+const course = {
+    coursename: "Javscript",
+    price:999,
+    name:"hitesh"
+}
+
+const {coursename :cname}= course
+console.log(cname)
+console.log(course) 
+
+// {
+//    " name" : "abhimanyu",
+//     "courseName":"jscouse",
+//     "price":free
+// }
+
+[
+    {},
+    {},
+    {}
+]
