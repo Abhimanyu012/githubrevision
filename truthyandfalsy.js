3f036afeb15575823b257b3d891d677e06341ab0
Abhimanyu <abhimanyucode2021@gmail.com>
2024-09-30 21:06:06 +0530

    truthy &flsy

---
+++ truthyandfalsy.js
@@ -0,0 +1,18 @@
+// falsy value
+// false , 0 ,-0 ,BigInt 0n , "" (without space), null , undeffined  ,NaN
+
+// truthy
+
+// "0" , 'false' , " "(with space), [], { }, function(){}
+
+// const array = []
+// if (array.length===0){
+//     console.log("array is empty")
+// }
+
+// const obj = {}
+
+// if (Object.keys(obj).length ===0){
+
+//     console .log ("object is empty")
+// }
