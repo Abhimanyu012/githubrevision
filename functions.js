0e647a4c304acd431b7867b057c1845ff53e5060
Abhimanyu <abhimanyucode2021@gmail.com>
2024-09-30 11:42:25 +0530

    send

---
+++ functions.js
@@ -0,0 +1,13 @@
+// function saymyanme(){
+//     console.log("A")
+//     console.log("M")
+//     console.log("A")
+//     console.log("N")
+// }
+// console.log(saymyanme())
+
+function addTwonumbers (num1,num2){
+    console.log(num1 +num2)
+}
+addTwonumbers(5,6)
+// console.log(addTwonumbers())
