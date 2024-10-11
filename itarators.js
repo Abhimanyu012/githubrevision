3b11b32d70363b6275aa0ab7c91bbb1d054e3327
Abhimanyu <abhimanyucode2021@gmail.com>
2024-10-11 16:46:59 +0530

    uploaded

---
+++ itarators.js
@@ -0,0 +1,23 @@
+
+// const myarr = [1,2,3,4,5]
+
+// for ( let i = 0 ; i <= myarr.length ; i ++){
+//     const element = i
+//     console.log (element)
+// }
+
+// for ( let i = 1 ; i <= 10 ; i ++){
+//     console.log(`outer loop: ${i}` )
+//     for( let j = 1 ; j <=10 ; j++){
+
+//         // console.log(`Inner loop value ${j} and inner lop ${i}`)
+//    console .log( i + ' X ' + j + ' = ' + i * j);
+//     } }
+
+
+// do while loop and while 
+
+ const arr = " abhimanyu"
+for (const num of arr) {
+    console.log(num);
+}
